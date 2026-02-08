db697fa9bbd06222508ec82570cedcbe54c7ef1c
Ivy Mahncke <imahncke@olin.edu>
2026-02-07 20:33:31 -0800

    extra main TODOs for kalman filter

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ Main file for running the simulator.
 
 from environment import Environment
 from robot import Robot
+from kalman_filter import KalmanFilter
 from utils import Position, Pose, Landmark, Bounds
 
 if __name__ == "__main__":
@@ -26,6 +27,12 @@ if __name__ == "__main__":
     # set up the robot
     robot = Robot(env)
 
+    # set up the Kalman Filter
+    kf = KalmanFilter(
+        dt,
+        initial_robot_pose,
+    )
+
     # set up timekeeping
     # TODO: set the total_seconds variable to however long you want the simulator to run (not real-time!)
     total_seconds = None
@@ -34,11 +41,13 @@ if __name__ == "__main__":
     # set up logging
     ground_truth_history = []
     sensor_data_history = []
+    kalman_filter_history = []
 
     # set up input filepath and output filepaths
     input_commands_filepath = ""
     output_ground_truth_filepath = ""
     output_sensor_data_filepath = ""
+    output_kalman_filter_filepath = ""
 
     # open up the instructions, pop the first
     with open(input_commands_filepath, "r") as cmd:
@@ -47,6 +56,10 @@ if __name__ == "__main__":
             # TODO: take a ground truth snapshot and add it to the history
 
             # TODO: take sensor measurements and add it to the history
+
+            # TODO: call the Kalman Filter prediction step
+
+            # TODO: call the Kalman Filter update step if new sensor data is available
 
             # TODO: retrieve the next motor command from the input file
 
@@ -58,3 +71,6 @@ if __name__ == "__main__":
 
     with open(output_sensor_data_filepath, "w") as sensor_data:
         # TODO: write sensor_data_history to a file
+
+    with open(output_kalman_filter_filepath, "w") as kf_data:
+        # TODO: write kalman_filter_history to a file
